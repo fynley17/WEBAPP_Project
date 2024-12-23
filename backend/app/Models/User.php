a19cdb0ab9448a07759c7326af638d2972f6cba3
fynley17 <62519801+fynley17@users.noreply.github.com>
2024-12-23 17:23:44 +0000

    adding search by username in model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,8 +2,22 @@
 namespace App\Models;
 
 class User extends Model {
+
+    // Fetch all users
     public static function all() {
         $stmt = self::getDB()->query("SELECT * FROM users");
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    // Fetch user by username
+    public static function find($username) {
+        // Validation
+        if (!preg_match("/^[a-zA-Z0-9_-]{3,30}$/",$username)){
+            throw new \InvalidArgumentException('Invalid username format');
+        }
+        $stmt = self::getDB()->query("SELECT * FROM users WHERE username = :username");
+        $stmt->bindValue(':username', $username, \PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 }
