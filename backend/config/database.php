fa25859bc54e6baf709a770d0b639e48ed252b4d
fynley17 <62519801+fynley17@users.noreply.github.com>
2024-12-23 13:29:16 +0000

    adding database connection

---
+++ backend/config/database.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ );
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$name = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
+try {
+    $dsn = "mysql:host=$host;dbname=$name";
+    $pdo = new PDO( $dsn, $user,$password );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    echo"connection successful";
+} catch (PDOException $e) {
+    echo "connection failed". $e->getMessage();
+}
