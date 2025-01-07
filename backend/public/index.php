d6e885022d8133ad36b6d98f6842a0f8648ca269
fynley17 <62519801+fynley17@users.noreply.github.com>
2025-01-07 18:13:00 +0000

    Update index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -33,20 +33,20 @@ else if ($uri === '/api/courses' && $method === 'GET'){
     $controller = new CourseController();
     $controller->getCourses();
 }else if (preg_match('/^\/api\/courses\/(\d+)$/', $uri, $matches) && $method === 'GET'){
-    $controller = new UserController();
-    $controller->getUserByID($matches[1]);
+    $controller = new CourseController();
+    $controller->getCourseByID($matches[1]);
 } else if (preg_match('/^\/api\/courses\/([^\/]+)$/', $uri, $matches) && $method === 'GET') {
-    $controller = new UserController();
-    $controller->getUserByUsername($matches[1]);
+    $controller = new CourseController();
+    $controller->getCourseByTitle($matches[1]);
 } else if (preg_match('/^\/api\/courses\/(\d+)$/', $uri, $matches) && $method === 'DELETE'){
-    $controller = new UserController();
-    $controller->deleteUser($matches[1]);
+    $controller = new CourseController();
+    $controller->deleteCourse($matches[1]);
 } else if ($uri === '/api/courses' && $method === 'POST') {
-    $controller = new UserController();
-    $controller->createUser();
+    $controller = new CourseController();
+    $controller->createCourse();
 } else if (preg_match('/^\/api\/courses\/(\d+)$/', $uri, $matches) && $method === 'PATCH'){
-    $controller = new UserController();
-    $controller->updateUser($matches[1]);
+    $controller = new CourseController();
+    $controller->updateCourse($matches[1]);
 // Assignment routes
 else if ($uri === '/api/assignments' && $method === 'GET'){
     $controller = new AssignmentController();
