aff836cb21aab461476527e67f244fe24d9e33fc
fynley17 <62519801+fynley17@users.noreply.github.com>
2025-01-12 16:13:55 +0000

    adding search and delete fo rassignments

--- backend/app/Models/Assignment.php
+++ backend/app/Models/Assignment.php
@@ -7,7 +7,7 @@ class Assignment extends Model
 
     public static function all()
     {
-        $stmt = self::getDB()->query("SELECT * FROM assignments");
+        $stmt = self::getDB()->query("SELECT assignmentID,users.username,courses.cTitle,courses.cDate,courses.cDuration,courses.currentAttendence FROM assignments JOIN users ON users.userID = assignments.userID JOIN courses ON courses.courseID = assignments.courseID");
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
@@ -27,7 +27,7 @@ class Assignment extends Model
             throw new \InvalidArgumentException('Invalid assignment ID');
         }
 
-        $stmt = self::getDB()->prepare("SELECT * FROM assignments WHERE assignmentID = :id");
+        $stmt = self::getDB()->prepare("SELECT assignmentID,users.username,courses.cTitle,courses.cDate,courses.cDuration,courses.currentAttendence FROM assignments JOIN users ON users.userID = assignments.userID JOIN courses ON courses.courseID = assignments.courseID WHERE assignmentID = :id");
         $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetch(\PDO::FETCH_ASSOC);
