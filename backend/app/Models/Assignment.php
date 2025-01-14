a3f567272128c50def19f74d1ef2317d03d0314c
fynley17 <62519801+fynley17@users.noreply.github.com>
2025-01-14 09:36:03 +0000

    adding create for assignment

--- backend/app/Models/Assignment.php
+++ backend/app/Models/Assignment.php
@@ -46,6 +46,23 @@ class Assignment extends Model
         return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 
+    public static function create($data){
+        if (empty($data['userID']) || !is_numeric($data['userID'])){
+            throw new \InvalidArgumentException('Invalid userID format');
+        }
+        if (empty($data['courseID']) || !is_numeric($data['courseID'])){
+            throw new \InvalidArgumentException('invalid courseID format');
+        }
+
+        $userID = htmlspecialchars(trim($data['userID']), ENT_QUOTES, 'utf-8');
+        $courseID = htmlspecialchars(trim($data['courseID']), ENT_QUOTES, 'utf-8');
+
+        $stmt = self::getDB()->prepare("INSERT INTO `assignments`(`userID`, `courseID`) VALUES (:userID,:userID)");
+        $stmt->bindvalue(':userID',$userID, \PDO::PARAM_INT);
+        $stmt->bindvalue(':courseID',$courseID, \PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
     public static function validUsername($username)
     {
         return preg_match("/^[a-zA-Z0-9_-]{3,30}$/", $username);
