f5ddcaeef8e902d5fc8df5326bd109ba83084ffb
fynley17 <62519801+fynley17@users.noreply.github.com>
2025-01-23 22:04:03 +0000

    updating

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . 'backend/vendor/autoload.php';
+require_once __DIR__ . '../vendor/autoload.php';
 
 use App\Controllers\UserController;
 use App\Controllers\CourseController;
