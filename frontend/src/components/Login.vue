eaee01b68d436df6661d5ad21ec731e14e5db9ba
fynley17 <62519801+fynley17@users.noreply.github.com>
2025-01-27 19:05:48 +0000

    learning

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,45 @@
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const name = ref('')
+    const el = ref()
+    const submitForm = () => {
+      console.log(`Form submitted! Name = ${name.value}`)
+    }
+
+    onMounted(() => {
+      el.value.focus()
+    })
+
+    return {
+      name,
+      submitForm,
+      el,
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <input ref="el" type="text" placeholder="Username" v-model="name" />
+    <div>
+      <button @click="submitForm" v-bind:disabled="name.length == 0">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+input[type='text'] {
+  padding: 5px;
+}
+
+button {
+  margin-top: 20px;
+}
+</style>
