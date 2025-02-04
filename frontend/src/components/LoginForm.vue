88e6032740b5257dd57226e61996e15202fd7fe2
Fynley Eastwood-geary <62519801+fynley17@users.noreply.github.com>
2025-02-04 19:59:56 +0000

    adding login page

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="row mt-5">
+            <div class="col">
+                <form class="bg-dark p-4 rounded">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                        <label for="username">Username</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="text" name="password" class="form-control" id="password" placeholder="Password">
+                        <label for="password">Password</label>
+                    </div>
+                    <button class="btn btn-primary form-control">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+    data() {
+        return {
+            input: {
+                username: "",
+                password: ""
+            }
+        }
+    }
+}
+</script>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,20 +1,16 @@
+<script>
+import LoginForm from '../components/LoginForm.vue'
+
+export default {
+    name: 'Login',
+    components: {
+        LoginForm
+    }
+}
+</script>
+
 <template>
-    <div class="flex justify-center items-center min-h-screen">
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+    <div>
+        <LoginForm />
     </div>
 </template>
