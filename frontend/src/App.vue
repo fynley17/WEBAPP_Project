284e70cc85ebdf74173efadd17bd94629f0956d5
fynley17 <62519801+fynley17@users.noreply.github.com>
2025-02-04 09:59:40 +0000

    fixing vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div v-for="user in users" :key="user.id">
-    <h2>{{ user.userID }} {{ user.username }}</h2>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      users:[]
-    }
-  },
-  mounted() {
-    axios
-      .get('http://localhost/api/users')
-      .then((response) => {
-        this.users = response.data
-      })
-  }
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-</script>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
