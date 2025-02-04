088ac312198bcb60a06f484506d13070ad02fa83
Fynley Eastwood-geary <62519801+fynley17@users.noreply.github.com>
2025-02-04 19:48:18 +0000

    setting up basic routing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <div v-for="user in users" :key="user.id">
-    <h2>{{ user.userID }} {{ user.username }}</h2>
-  </div>
+  <router-view />
 </template>
 
 <script>
-import api from './services/api'; // Ensure this is your Axios instance
-
 export default {
-  data() {
-    return {
-      users: []
-    };
-  },
-  mounted() {
-    api.get('/users') // Corrected syntax
-      .then((response) => {
-        this.users = response.data;
-      })
-      .catch((error) => {
-        console.error('Error fetching users:', error);
-      });
-  }
+  name: "App"
 };
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '@/views/Login.vue';
-import AdminDashboard from '@/views/AdminDashboard';
-import StaffDashboard from '@/views/StaffDashboard';
+import Login from '../views/Login.vue';
+// import AdminDashboard from '/../views/AdminDashboard';
+// import StaffDashboard from '/../views/StaffDashboard';
 
 const routes = [
-    { path: '/', component: Login },
-    { path: '/admin', component: AdminDashboard, meta: { requiresAuth: true, role: 'admin' } },
-    { path: '/staff', component: StaffDashboard, meta: { requiresAuth: true, role: 'staff' } }
+    { path: '/', component: Login }
+    // { path: '/admin', component: AdminDashboard, meta: { requiresAuth: true, role: 'admin' } },
+    // { path: '/staff', component: StaffDashboard, meta: { requiresAuth: true, role: 'staff' } }
 ];
 
 const router = createRouter({
